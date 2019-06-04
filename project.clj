99ff22638e959b9a984ae16a4cbaa86971bce04b
Lamar Payson <glpayson@gmail.com>
2019-06-03 21:05:55 -0400

    Update project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject ecs-cost-estimator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Parse ECS terraform file and calculate estimated min and max cost per month."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
