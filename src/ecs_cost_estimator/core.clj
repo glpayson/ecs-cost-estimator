39c9bf4b20602781bf057aed81366a8daa2af844
Lamar Payson <glpayson@gmail.com>
2019-06-03 21:01:12 -0400

    Add docstrings.

--- src/ecs_cost_estimator/core.clj
+++ src/ecs_cost_estimator/core.clj
@@ -4,6 +4,8 @@
             [ecs-cost-estimator.hcl-parser :as parser]))
 
 (defn- tasks-per-instance
+  "Calculates how many tasks of an ECS service will fit on a given EC2 instance bounded by either
+  memory or vcpus"
   [req-vcpus req-mem instance-vcpus instance-mem]
   (int
     (Math/floor
@@ -11,10 +13,13 @@
            (/ instance-mem req-mem)))))
 
 (defn- cost-month
+  "Minimum number of EC2 instances required to host the tasks-per-instance number of tasks times the
+  cost per month of the EC2 instance type."
   [capacity tasks-per-instance]
   (* ecs/standard-cost-month (Math/ceil (/ capacity tasks-per-instance))))
 
 (defn- add-costs
+  "Add cost per month for minimum and maximum capacity of the ECS service"
   [{:keys [vcpus, memory-gb, min-capacity, max-capacity] :as service}]
   (let [tasks-per-instance
         (tasks-per-instance vcpus memory-gb ecs/standard-vcpus ecs/standard-memory)]
