7089d9e53a61137c4c473da500cb6dbd7d844d13
Lamar Payson <glpayson@gmail.com>
2019-06-03 21:54:59 -0400

    Move casing to parse function.

--- src/ecs_cost_estimator/hcl_parser.clj
+++ src/ecs_cost_estimator/hcl_parser.clj
@@ -12,7 +12,8 @@
         (.get % "module")
         (.get % "ecs-service")
         (assoc (into {} %) "key" "value")
-        (keywordize-keys %)))
+        (keywordize-keys %)
+        (cske/transform-keys csk/->kebab-case-keyword %)))
 
 (defn- string-to-gbs
   [str]
@@ -22,8 +23,7 @@
   "Parse relevant fields from an ECS Terraform file"
   [filename]
   (as-> (parse-ecs-specs filename) %
-        (select-keys % [:application :memory :cpu :min_capacity :max_capacity])
-        (cske/transform-keys csk/->kebab-case-keyword %)
+        (select-keys % [:application :memory :cpu :min-capacity :max-capacity])
         (update % :memory #(string-to-gbs %))
         (update % :cpu #(string-to-gbs %))
         (update % :min-capacity #(Integer/parseInt %))
